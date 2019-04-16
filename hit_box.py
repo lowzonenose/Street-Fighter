c5caf6ff5555da06fe996f8219e987b200ab513a
Pollux26042001 <47292880+Pollux26042001@users.noreply.github.com>
2019-04-16 21:10:58 +0200

    Add files via upload

---
+++ hit_box.py
@@ -0,0 +1,49 @@
+
+hit_box = {
+	"r_idle": (3, 0, 34, 78),
+	"r_walking_left": (6, 0, 30, 79),
+	"r_walking_right": (3, 0, 34, 77),
+	"r_jump_up": (0, 0, 32, 89),
+	"r_jump_down": (0, 0, 30, 67),
+	"r_crouch": (7, 0, 34, 54),
+	"r_h_punch": (11, 0, 36, 80),
+	"r_jump_punch": (0, 0, 32, 50),
+	"r_crouch_l_punch": (7, 0, 34, 51),
+	"r_l_kick": (0, 1, 34, 35),
+	"r_crouch_h_kick": (16, 0, 34, 45),
+	"r_jump_l_kick": (7, 1, 21, 81),
+
+
+
+	"l_idle": (8, 0, 31, 78),
+	"l_walking_left": (5, 0, 34, 77),
+	"l_walking_right": (7, 0, 28, 79),
+	"l_jump_up": (0, 0, 32, 89),
+	"l_jump_down": (0, 0, 30, 67),
+	"l_crouch": (3, 0, 31, 53),
+	"l_h_punch": (24, 0, 36, 81),
+	"l_jump_punch": (18, 0, 27, 51),
+	"l_crouch_l_punch": (21, 0, 31, 52),
+	"l_l_kick": (26, 1, 40, 40),
+	"l_crouch_h_kick": (38, 0, 33, 46),
+	"l_jump_l_kick": (16, 0, 25, 81),
+
+
+
+	"attaque":{
+		"r_h_punch": (46, 14, 26, 10),
+		"r_jump_punch": (41, 23, 10, 9),
+		"r_crouch_l_punch": (43, 11, 19, 7),
+		"r_l_kick": (46, 0, 20, 14),
+		"r_crouch_h_kick": (67, 31, 22, 14),
+		"r_jump_l_kick": (32, 0, 13, 21),
+
+		"l_h_punch": (0, 14, 24, 9),
+		"l_jump_punch": (0, 22, 9, 10),
+		"l_crouch_l_punch": (0, 11, 19, 7),
+		"l_l_kick": (0, 0, 17, 13),
+		"l_crouch_h_kick": (0, 30, 25, 15),
+		"l_jump_l_kick": (0, 0, 13, 19),
+
+	}
+}
---
+++ setting.py
@@ -0,0 +1,39 @@
+import pygame
+
+
+setting = {
+	"l_ecran": 1200,
+	"titre": "STREET FIGHTER",
+	"fps": 120,
+
+	"posX_fond": 0,
+	"speed": 3.5,
+	"delai_reset_touche": 0.15,
+	"vie": 100,
+
+
+	"touche_joueur2": {
+		"right": pygame.K_RIGHT,
+		"left": pygame.K_LEFT,
+		"up": pygame.K_UP,	
+		"down": pygame.K_DOWN,
+		"h_punch": pygame.K_KP1,
+		"l_kick": pygame.K_KP2,
+		"blocking": pygame.K_KP3,
+		"victory1": pygame.K_KP7,
+		"victory2": pygame.K_KP8,
+	},
+	"touche_joueur1": {
+		"right": pygame.K_d,
+		"left": pygame.K_a,
+		"up": pygame.K_w,
+		"down": pygame.K_s,
+		"h_punch": pygame.K_t,
+		"l_kick": pygame.K_y,
+		"blocking": pygame.K_u,
+		"victory1": pygame.K_g,
+		"victory2": pygame.K_h,
+	},
+
+
+}
